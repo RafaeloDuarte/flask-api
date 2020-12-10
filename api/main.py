86053370bd1928c642fab6aac24eb02afaa25748
Rafael Duarte <rduarte@sccalpha.local>
2020-12-10 17:50:33 -0300

    first commit

--- api/main.py
+++ api/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     request_method = request.method
-    return render_template('hello.html', request_method = request_method)
+    if request_method == 'POST':
+        print('---------------')
+        print(request.form)
+        print('---------------')
+        return redirect(url_for('name'))
+    return render_template('helloform.html', request_method = request_method)
 
+@app.route('/name')
+def name():
+    return 'name'
 
 if __name__ == "__main__":
     app.run(debug = True)

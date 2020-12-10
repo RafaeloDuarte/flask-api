4119d681474bca6cc66c9bd0aa23d0d3e34a2b70
Rafael Duarte <rduarte@sccalpha.local>
2020-12-10 17:02:09 -0300

    first commit

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hi():
-    return 'Hi'
+    return render_template('hello.html')
 
 @app.route('/<string:name>')
 def hello_world(name):

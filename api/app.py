299852061305536256e978d40572b29b73d56685
Rafael Duarte <rduarte@sccalpha.local>
2020-12-10 22:09:35 -0300

    .

--- api/app.py
+++ api/app.py
@@ -7,16 +7,21 @@ app.config['SECRET_KEY'] = 'password'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
 db = SQLAlchemy(app)
 
-class TodoModel(db.model):
-    pass
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(240))
+
+    def __str__(self):
+        return f'{self.content}, {self.id}'
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     request_method = request.method
+    todo = TodoModel.query.all()
     if request_method == 'POST':
         first_name = request.form['name']
         return redirect(url_for('name', first_name=first_name))
-    return render_template('helloform.html', request_method = request_method)
+    return render_template('helloform.html', request_method = request_method, todo=todo)
 
 @app.route('/name/<string:first_name>')
 def name(first_name):
@@ -26,7 +31,10 @@ def name(first_name):
 def todo():
     todo_form = Todo()
     if todo_form.validate_on_submit():
-        print(todo_form.content.data)
+        todo = TodoModel(content=todo_form.content.data)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
     return render_template('todo.html', form=todo_form)
 
 if __name__ == "__main__":
